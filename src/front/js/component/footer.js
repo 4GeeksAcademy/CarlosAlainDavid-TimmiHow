d71ed68fa89e3f228b1b1a2c38487feaf4098ade
NDavidGR <146215925+NDavidGR@users.noreply.github.com>
2024-04-04 00:33:11 +0000

    Subiendo logo footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import {logo-footer} from "../../img/logo-footer-05.png";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top p-5">
+		<p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 Timmihow</p>
+
+		<a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+			<img src='https://drive.google.com/file/d/1cT9Bk6hXwDiAZEIDxLPaskiG5ggxilPz/view?usp=sharing' alt='timmihow-logo'/>
+			<img src={logo-footer} />
+		</a>
+
+		<ul class="nav col-md-4 justify-content-end">
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Why Us</a></li>
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+			<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Categories</a></li>
+		</ul>
+  </footer>
 );
