ffcbc17c3244cc876f3a6b33af87e5a76a4e84b5
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-04-11 00:00:54 +0000

    "Who we are take2"

--- src/front/js/component/aboutUs.js
+++ src/front/js/component/aboutUs.js
@@ -5,17 +5,45 @@ import GrayBg from "../../img/gray-background.png"
 const AboutUs = () => {
     return (
         <div className="bg-light">
-            <div className="Who-we-Are row">Who we are</div>
             <div className="container py-5">
-                <div className="row h-100 align-items-center py-5">
-                    <div className="col-lg-6">
+                <div className="row justify-content-center align-items-center mb-5">
+                    <div className="col text-center">
+                        <h2 className="Who-we-Are">Who we are</h2>
                     </div>
+                </div>
+                <div className="row d-flex justify-content-between align-items-center g-5">
                     <div className="col-lg-6">
+                        <p className="Introductory-Text lead">
+                            Different country... new life... new challenges, right?
+                            From knowing what to do or even where to start can certainly be something
+                            worth learning and discovering. We're here to help guide you through all the basic knowledge
+                            and skills you need to get yourself ready and confident for your life in this country.
+                            Let's do this! We can make it!
+                        </p>
+                    </div>
+                    <div>
+                        <div className="col-lg-6 position-absolute ms-lg-auto img-fluid">
+                            <img
+                                src={StudentnoDeco}
+                                className="img-fluid"
+                                alt="Bootstrap Themes"
+                                style={{ position: 'absolute', top: '80%', left: '50%', transform: 'translateY(-100%)' }}
+                            />
+                        </div>
+                        <div className="col-lg-6 d-block ms-lg-auto img-fluid">
+                            <img
+                                src={GrayBg}
+                                className="img-fluid"
+                                alt="Bootstrap Themes"
+                                />
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+
+
 
 export default AboutUs
