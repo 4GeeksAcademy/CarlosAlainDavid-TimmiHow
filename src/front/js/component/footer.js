0e2b73ba5d3ced6b1252fd45b18d5b695bae7431
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-04-12 23:31:08 +0000

    "footer completed"

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,19 +1,158 @@
-import React, { Component } from "react";
-import logoFooter from "../../img/logo-footer-05.png";
+import React from "react";
+import FooterStyles from "../../styles/footer.css"
+import LogoOutline from "../../img/timmihow-name-outline.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faInstagram, faLinkedinIn, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
-export const Footer = () => (
-	<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top p-5">
-		<p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 Timmihow</p>
+const Footer = () => {
+  return (
+    <div className="d-flex justify-content-center bg-our-blue text-light">
+      <div className="container">
+        <footer className="py-5">
+          <div className="row">
+            <div className="col-6 col-md-2 mb-3">
+              <h5 className="our-yellow">Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <a href="#" className="text-light nav-link p-0 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="text-light nav-link p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="text-light nav-link p-0 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="text-light nav-link p-0 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6 col-md-2 mb-3">
+              <h5 className="our-yellow">Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-6 col-md-2 mb-3">
+              <h5 className="our-yellow">Section</h5>
+              <ul className="nav flex-column">
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li className="nav-item mb-2">
+                  <a href="#" className="nav-link text-light p-0 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-5 offset-md-1 mb-3">
+              <form>
+                <h5>Subscribe to our newsletter</h5>
+                <p>Monthly digest of what's new and exciting from us.</p>
+                <div className="d-flex flex-column flex-sm-row w-100 gap-2">
+                  <label htmlFor="newsletter1" className="visually-hidden">
+                    Email address
+                  </label>
+                  <input
+                    id="newsletter1"
+                    type="text"
+                    className="form-control"
+                    placeholder="Email address"
+                  />
+                  <button className="subscribe-btn btn btn-warning" type="button">
+                    Subscribe
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="d-flex align-items-center flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+            <img src={LogoOutline} width="110px" />
+            <p>© 2024 Timmihow, LLC All rights reserved.</p>
+            <ul className="list-unstyled d-flex">
+              <li className="ms-3">
+                <a className="link-body-emphasis" href="#">
+                  <FontAwesomeIcon className="fs-2 text-light" icon={faTwitter} />
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-body-emphasis" href="#">
+                <FontAwesomeIcon className="fs-2 text-light" icon={faFacebook} />
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-body-emphasis" href="#">
+                <FontAwesomeIcon className="fs-2 text-light" icon={faInstagram} />
+                </a>
+              </li>
+              <li className="ms-3">
+                <a className="link-body-emphasis" href="#">
+                <FontAwesomeIcon className="fs-2 text-light" icon={faLinkedinIn} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
 
-		<a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-			<img src='https://drive.google.com/file/d/1cT9Bk6hXwDiAZEIDxLPaskiG5ggxilPz/view?usp=sharing' alt='timmihow-logo'/>
-			<img src={logoFooter} />
-		</a>
+    </div>
 
-		<ul class="nav col-md-4 justify-content-end">
-			<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Why Us</a></li>
-			<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-			<li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Categories</a></li>
-		</ul>
-  </footer>
-);
+  );
+}
+
+export default Footer
