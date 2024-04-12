7b06a1c96ab6a88e20178cfdb9785f50bdbe1c71
Carlos Eduardo Escalante Montilla <129804004+CarlosEscalanteM@users.noreply.github.com>
2024-04-12 17:11:22 +0000

    "Added badge and ourAdvantages components"

--- src/front/js/component/ourAdvantages.js
+++ src/front/js/component/ourAdvantages.js
@@ -1,36 +1,43 @@
 import React from "react"
+import Badge from "./badge"
 import CheckBadge from "../../img/check-badge.png"
 import RotatingGear from "../../img/rotating-gear.png"
 import Clock from "../../img/clock.png"
 import SupportiveHands from "../../img/supportive-hands.png"
 
 const OurAdvantages = () => {
+    const ImageList = [{
+        image: CheckBadge,
+        title: "Verified Tutors",
+        description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    },
+    {
+        image: RotatingGear,
+        title: "Reliable Content",
+        description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    },
+    {
+        image: Clock,
+        title: "Time Flexibility",
+        description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    },
+    {
+        image: SupportiveHands,
+        title: "Support",
+        description: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    },
+    ]
+
     return (
-        <div className="our-pink">
+        <div className="background-pink">
             <div className="px-5 py-5">
                 <div className="row justify-content-center align-items-center mb-3">
                     <div className="our-advantages col text-center">
-                        <h1 className="Section-3"><span className="font-size-headers light">Our Advantages</span></h1>
+                        <h1 className="Section-3"><span className="font-size-headers our-yellow">Our Advantages</span></h1>
                     </div>
                 </div>
                 <div className="our-advantages-container d-flex justify-content-between align-items-center">
-                    <div className="card card-background shadow p-3 col-lg-6"
-                    style={{marginBottom: "9%", borderRadius: "15px" }}>
-                        <p className="Introductory-Text text-light lead fading text-align-center">
-                            Different country... new life... new challenges, right?
-                            From knowing what to do or even where to start can certainly be something
-                            worth learning and discovering. We're here to help guide you through all the basic knowledge
-                            and skills you need to get yourself ready and confident for your life in this country.
-                            Let's do this! We can make it!
-                        </p>
-                    </div>
-                    <div className="col-lg-6 d-block">
-                        <img
-                            src={StudentNoDeco}
-                            className="img-fluid"
-                            alt="Bootstrap Themes"
-                        />
-                    </div>
+                    {ImageList.map(badge => <Badge image={badge.image} title={badge.title} description={badge.description} />)}
                 </div>
             </div>
         </div>
