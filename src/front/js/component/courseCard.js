65ff65cc7adf4fc4635adea5b184fa03d6b62d6a
NDavidGR <146215925+NDavidGR@users.noreply.github.com>
2024-04-25 00:31:18 +0000

    Carrusel y vista consumer

--- src/front/js/component/courseCard.js
+++ src/front/js/component/courseCard.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const CourseCard = ({ image, description, price, sessionCount }) => {
+const CourseCard = ({ image, description, price, sessionCount, width, flex }) => {
     return (
-        <div className="d-block" style={{
-            width: "33%"
+        <div className={!flex && "d-block" || "d-flex flex-column align-items-center"} style={{
+            width: width && width || "33%"
         }}>
             <img className="img-fluid" src={image} />
             <p>{description}</p>
             <div className="d-flex justify-content-between align-items-center">
-                <p>Price: <span className="text-danger">USD$ </span>{price}</p>
+                {price && <p>Price: <span className="text-danger">USD$ </span>{price}</p>}
                 <p>Total of Sessions:{sessionCount}</p>
                 <button className="btn btn-warning">Book class</button>
             </div>
